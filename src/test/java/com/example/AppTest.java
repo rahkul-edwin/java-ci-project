ff94e19439f9eb05cde224c39e5f3115943a8b3f
Edwin rahkul <edwinrahkul28@gmail.com>
2026-09-20 12:44:18 +0530

    Add files via upload

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+
+        int result = 10 + 20;
+
+        assertEquals(30, result);
+    }
+}
